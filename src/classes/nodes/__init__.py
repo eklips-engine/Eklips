43958ba4e6c8c29da70495900c056e3bd8977bae
ZeeAy <zagameserx90@gmail.com>
2025-12-12 14:31:54 +0100

    CollisionBox

--- src/classes/nodes/__init__.py
+++ src/classes/nodes/__init__.py
@@ -3,11 +3,11 @@ print(" ~ Importing all nodes")
 from classes.nodes.node  import *
 from classes.nodes.timer import *
 
-from classes.nodes.gui.canvasitem  import *
-from classes.nodes.gui.colorrect   import *
-from classes.nodes.gui.label       import *
-from classes.nodes.gui.window      import *
-from classes.nodes.gui.sprite      import *
-from classes.nodes.gui.parallax    import *
-
-from classes.nodes.gui.mediaplayer import *
+from classes.nodes.gui.canvasitem   import *
+from classes.nodes.gui.colorrect    import *
+from classes.nodes.gui.label        import *
+from classes.nodes.gui.window       import *
+from classes.nodes.gui.sprite       import *
+from classes.nodes.gui.parallax     import *
+from classes.nodes.gui.collisionbox import *
+from classes.nodes.gui.mediaplayer  import *
